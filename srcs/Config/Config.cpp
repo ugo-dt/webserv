09554292b9f67c54be2af35422f6f43250828a1a
Ugo DANIEL-THOMAS <ugdaniel@e2r8p5.clusters.42paris.fr>
2022-10-06 15:08:16 +0200

    webserv

--- srcs/Config/Config.cpp
+++ srcs/Config/Config.cpp
@@ -6,18 +6,15 @@
 /*   By: ugdaniel <ugdaniel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/21 15:50:20 by ugdaniel          #+#    #+#             */
-/*   Updated: 2022/10/04 14:57:42 by ugdaniel         ###   ########.fr       */
+/*   Updated: 2022/10/06 14:53:06 by ugdaniel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Config.hpp"
 
-Config::Config(const char *path)
+Config::Config(const char *filename)
+	: _config_path(filename)
 {
-	if (path)
-		_config_path = path;
-	else
-		_config_path = DEFAULT_CONFIG_PATH;
 }
 
 Config::~Config()
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ugdaniel <ugdaniel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/03 19:43:26 by ugdaniel          #+#    #+#             */
-/*   Updated: 2022/10/04 15:02:45 by ugdaniel         ###   ########.fr       */
+/*   Updated: 2022/10/06 14:55:19 by ugdaniel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,24 @@ int	main(int argc, char **argv)
 		std::cerr << "webserv: too many arguments" << std::endl;
 		return (1);
 	}
-	config = new Config(argv[1]);
+	if (argv[1])
+		config = new Config(argv[1]);
+	else
+		config = new Config(DEFAULT_CONFIG_PATH);
 	std::cout << "File path: " << config->config_path() << std::endl;
+
+/* Since MacOS doesn’t implement write() the same way as other Unix
+	OSes, you are allowed to use fcntl().
+	You must use file descriptors in non-blocking mode in order to get a
+	behavior similar to the one of other Unix OSes.
+	However, you are allowed to use fcntl() only as follows:
+	fcntl(fd, F_SETFL, O_NONBLOCK);
+	Any other flag is forbidden. */
+# if defined(__linux__)
+	std::cout << "Linux" << std::endl;
+# else
+	std::cout << "macOS" << std::endl;
+# endif
 
 	// parse
 	// run
