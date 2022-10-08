755af80e0c78f03fcd566400827ef243ea65d1db
Ugo DANIEL-THOMAS <ugdaniel@42.student.fr>
2022-10-08 11:58:43 +0200

    ugo

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ugdaniel <ugdaniel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/03 19:43:26 by ugdaniel          #+#    #+#             */
-/*   Updated: 2022/10/06 14:55:19 by ugdaniel         ###   ########.fr       */
+/*   Updated: 2022/10/08 11:58:28 by ugdaniel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,9 @@ int	main(int argc, char **argv)
 		config = new Config(DEFAULT_CONFIG_PATH);
 	std::cout << "File path: " << config->config_path() << std::endl;
 
-/* Since MacOS doesn’t implement write() the same way as other Unix
-	OSes, you are allowed to use fcntl().
-	You must use file descriptors in non-blocking mode in order to get a
-	behavior similar to the one of other Unix OSes.
-	However, you are allowed to use fcntl() only as follows:
-	fcntl(fd, F_SETFL, O_NONBLOCK);
+/* Since MacOS doesn’t implement write() the same way as other Unix OSes, you are allowed to use fcntl().
+	You must use file descriptors in non-blocking mode in order to get a behavior similar to the one of other Unix OSes.
+	However, you are allowed to use fcntl() only as follows: fcntl(fd, F_SETFL, O_NONBLOCK);
 	Any other flag is forbidden. */
 # if defined(__linux__)
 	std::cout << "Linux" << std::endl;
